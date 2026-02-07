6beabee2d283458185f9afe448057c93a75cd59b
Mohamad Badiezadegan <mbzadegan@gmail.com>
2026-02-07 14:40:03 -0800

    Add documentation for Obfuscated Doron C program
    Added comments explaining the purpose and origin of the code.

---
+++ Algorithms/Obfuscated_Doron.c
@@ -0,0 +1,19 @@
+/* Even C’s most ardent admirers admit that C code can be hard to read. The annual International Obfuscated C Code Contest actually encourages contestants to write the most confusing C programs possible. 
+The winners are truly baffling, as the 1990s “Best Small Program.”
+C Programming: A Modern Approach, By K.N.KING
+This program, written by Doron Osovlanski and Baruch Nissenbaum, prints all solutions to the Eight Queens problem (the problem of placing eight queens on a
+chessboard in such a way that no queen attacks any other queen). In fact, it works for any number of queens between four and 99. For more winning programs, visit
+www.ioccc.org, the contest’s website.
+
+*/
+
+#include <stdio.h>
+int v,i,j,k,l,s,a[99];
+int main()
+{
+ for(scanf("%d",&s);*a-s;v=a[j*=v]-a[i],k=i<s,j+=(v=j<s&&
+(!k&&!!printf(2+"\n\n%c"-(!l<<!j)," #Q"[l^v?(l^j)&1:2])&&
+++l||a[i]<s&&v&&v-i+j&&v+i-j))&&!(l%=s),v||(i==j?a[i+=k]=0:
+++a[i])>=s*k&&++a[--i])
+ ;
+}
